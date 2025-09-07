ed21887d8e5c095350695f43bc6e3916f1dbb4dc
Paulo Kinjo <v-paulokinjo@microsoft.com>
2025-09-07 00:01:23 +0000

    feat(prettier): setup prettier

--- index.ts
+++ index.ts
@@ -1,16 +1,16 @@
-import concurrently from "concurrently";
+import concurrently from 'concurrently';
 
 concurrently([
-  {
-    name: "server",
-    command: "bun dev",
-    cwd: "packages/server",
-    prefixColor: "blue",
-  },
-  {
-    name: "client",
-    command: "bun dev --host 0.0.0.0 --port 5173",
-    cwd: "packages/client",
-    prefixColor: "green",
-  },
+   {
+      name: 'server',
+      command: 'bun dev',
+      cwd: 'packages/server',
+      prefixColor: 'blue',
+   },
+   {
+      name: 'client',
+      command: 'bun dev --host 0.0.0.0 --port 5173',
+      cwd: 'packages/client',
+      prefixColor: 'green',
+   },
 ]);
--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,23 +1,23 @@
-import { useEffect, useState } from "react";
-import "./App.css";
-import { Button } from "./components/ui/button";
+import { useEffect, useState } from 'react';
+import './App.css';
+import { Button } from './components/ui/button';
 
 function App() {
-  const [message, setMessage] = useState("");
+   const [message, setMessage] = useState('');
 
-  useEffect(() => {
-    fetch("/api/hello")
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message))
-      .catch((err) => console.error("Error fetching message:", err));
-  }, []);
+   useEffect(() => {
+      fetch('/api/hello')
+         .then((res) => res.json())
+         .then((data) => setMessage(data.message))
+         .catch((err) => console.error('Error fetching message:', err));
+   }, []);
 
-  return (
-    <div className="p-4">
-      <p className="font-bold text-4xl">{message || "Loading..."}</p>
-      <Button>Click Me</Button>
-    </div>
-  );
+   return (
+      <div className="p-4">
+         <p className="font-bold text-3xl">{message || 'Loading...'}</p>
+         <Button>Click Me</Button>
+      </div>
+   );
 }
 
 export default App;
